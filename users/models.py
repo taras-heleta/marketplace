b7771efae4ed47726bd0bb9384a1cebd2118faf8
sandor4ik <mashkin.vanya@gmail.com>
2025-03-14 22:34:15 +0200

    Add new model CustomUser

---
+++ users/models.py
@@ -0,0 +1,22 @@
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+
+import uuid
+
+
+class Role(models.TextChoices):
+    USER = 'user', 'User'
+    ADMIN = 'admin', 'Admin'
+
+class CustomUser(AbstractUser):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    email = models.EmailField(unique=True)
+    full_name = models.CharField(max_length=255, blank=True, null=True)
+    phone_number = models.CharField(max_length=20, blank=True, null=True)
+    avatar = models.CharField(max_length=256, blank=True, null=True)
+    role = models.CharField(max_length=10, choices=Role.choices, default=Role.USER)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.email
