90aaa8ebedac909c006ec854e0b1566d8121e8a3
Taras Heleta <36954466+taras-heleta@users.noreply.github.com>
2025-03-19 14:24:53 +0200

    Update urls

--- core/urls.py
+++ core/urls.py
@@ -20,5 +20,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('health/', include('health.urls')),
-    path('api/', include('users.urls'))
+    path('api/', include('users.urls')),
 ]
